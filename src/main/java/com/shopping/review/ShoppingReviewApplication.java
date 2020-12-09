a79175bcd833306f976bea8bfb75228edcd5f951
Ghansham Zinge <zinge.sham@gmail.com>
2020-12-09 13:47:26 +0530

    First review service

---
+++ src/main/java/com/shopping/review/ShoppingReviewApplication.java
@@ -0,0 +1,13 @@
+package com.shopping.review;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShoppingReviewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShoppingReviewApplication.class, args);
+	}
+
+}
